416615606f64d9ecf4ab3c95fa7fd5a9ee94d180
Luke Snyder <luke.snyder@caa.com>
2018-06-30 10:45:26 -0400

    testing

--- models/stat.js
+++ models/stat.js
@@ -1,27 +1,28 @@
 const mongo = require('mongodb')
 
 exports.upsert = (req, statType, cb) => {
-  switch(statType) {
-    case 'team':
-      const statInfoList = req.body.teamStatInfoList;
-    case 'defense':
-      const statInfoList = req.body.playerDefensiveStatInfoList;
-    case 'rushing':
-      const statInfoList = req.body.playerRushingStatInfoList;
-    case 'passing':
-      const statInfoList = req.body.playerPassingStatInfoList;
-    case 'receiving':
-      const statInfoList = req.body.playerReceivingStatInfoList;
-    case 'kicking':
-      const statInfoList = req.body.playerKickingStatInfoList;
-    case 'punting':
-      const statInfoList = req.body.playerPuntingStatInfoList;
-  }
   mongo.connect('mongodb+srv://lsnyder:F51xtOAJYvqin5@free-dev-01-gwb63.mongodb.net/teams?retryWrites=true', function(err, client) {
     if (err) throw err;
     var db = client.db('stats')
     var bulk = db.collection('stat').initializeUnorderedBulkOp();
-
+    let statInfoList
+    switch(statType) {
+      case 'team':
+        statInfoList = req.body.teamStatInfoList;
+      case 'defense':
+        statInfoList = req.body.playerDefensiveStatInfoList;
+      case 'rushing':
+        statInfoList = req.body.playerRushingStatInfoList;
+      case 'passing':
+        statInfoList = req.body.playerPassingStatInfoList;
+      case 'receiving':
+        statInfoList = req.body.playerReceivingStatInfoList;
+      case 'kicking':
+        statInfoList = req.body.playerKickingStatInfoList;
+      case 'punting':
+        statInfoList = req.body.playerPuntingStatInfoList;
+    }
+    console.log("###", statInfoList)
     statInfoList.forEach(stat => {
       stat.type = statType;
       bulk.find({statId:stat.statId, type:stat.type}).upsert().updateOne(stat)
