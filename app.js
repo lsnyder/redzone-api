0d65240308f4f54e4aee66950e56252fb4d992d4
Luke Snyder <luke.snyder@caa.com>
2018-06-30 12:24:06 -0400

    y

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({limit: '50mb'}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(bodyParser.json({limit: '100mb'}))
+app.use(bodyParser.urlencoded({limit: '100mb', extended: true}))
 
 app.use('/', indexRouter);
 app.use('/players', playerRouter);
