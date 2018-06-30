61043a17daf9221c6d6eb39e46ca8f141c827253
Luke Snyder <luke.snyder@caa.com>
2018-06-30 12:26:15 -0400

    Body Parser

--- app.js
+++ app.js
@@ -19,12 +19,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({limit: '100mb'}))
-app.use(bodyParser.urlencoded({limit: '100mb', extended: true}))
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 app.use('/', indexRouter);
 app.use('/players', playerRouter);
