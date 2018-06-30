1efc4f7d2749a66c3049affd883aaf0935345d16
Luke Snyder <luke.snyder@caa.com>
2018-06-30 12:21:26 -0400

    Body Parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var scoreRouter = require('./routes/scores');
@@ -22,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 app.use('/', indexRouter);
 app.use('/players', playerRouter);
